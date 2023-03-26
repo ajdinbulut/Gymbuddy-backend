4761e6be121eec36a96f0988d995f47959d07276
Ajdin Bulut <ajdinubully@gmail.com>
2023-03-26 20:30:35 +0200

    added chat service,made api call to get chat

---
+++ Gymbuddy/Controllers/ChatController.cs
@@ -0,0 +1,22 @@
+﻿using Gymbuddy.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Gymbuddy.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ChatController : Controller
+    {
+        private readonly DB _db;
+        public ChatController(DB db)
+        {
+            _db = db;
+        }
+        [HttpGet("getChat")]
+        public IActionResult GetChat([FromQuery]ChatModel model)
+        {
+            var chat = _db.Chats.Where(x => x.UserSenderId == model.UserSender && x.UserReceiverId == model.UserReceiver).ToList();
+            return Ok(chat);
+        }
+    }
+}
---
+++ Gymbuddy/Models/ChatModel.cs
@@ -0,0 +1,8 @@
+﻿namespace Gymbuddy.Models
+{
+    public class ChatModel
+    {
+        public int UserSender { get; set; }
+        public int UserReceiver { get; set; }
+    }
+}
